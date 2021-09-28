1d5bd9c4bb45a3545ca50e51e4b8546be5e8726f
Ivan-mech99 <ibistrov90@icloud.com>
2021-09-28 10:33:38 +0300

    pt_bad

--- data/main.cpp
+++ data/main.cpp
@@ -176,7 +176,8 @@ void Boundary_conditions_test() {
 
 int main() {
   cout<<"Change1"<<endl;
-  cout<<"1-2"<<endl;
+  cout<<"1-2000000"<<endl;
+  cout<<"fuu-666"<<endl;
   Alloc_first_block();
   Alloc_3_blocks();
   Alloc_too_big_block();
